1a03e15976675fd2084f016681ffe4e61d79dcee
Jake Overall <joverall22@gmail.com>
2016-07-10 21:54:00 -0600

    Update image-service.js

--- app/components/image/image-service.js
+++ app/components/image/image-service.js
@@ -2,7 +2,7 @@ function ImageService (){
 	this.getImage =  function(callWhenDone){
 				// ^^^^^^^ How do you use this?
 		return $.get('http://www.splashbase.co/api/v1/images/random', function(res){
-			console.log('Image Data:', res.data)
+			console.log('Image Data:', res)
 			//What are you going to do with the image that comes back?
 		})
 	}
