60312c77947af998f2e09956a0c0cfa74ebe5fba
Jake Overall <joverall22@gmail.com>
2016-07-10 22:02:41 -0600

    Update todo-controller.js

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,5 +1,8 @@
 (function(){
-	// new up the TodoService
+	// new up the TodoService that has already been configured for your use
+	// There are two methods getTodos returns and array
+	// saveTodos accepts an array and stores it to your local storage
+	
 	
 	
 	
