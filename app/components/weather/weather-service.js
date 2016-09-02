1428b98b2dfe70937c14a9766604656ba9277be1
BertAllen <mrlaughingdragon@aol.com>
2016-09-02 16:03:51 -0600

    Changing to bcw-getter to buffer the https problem

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -1,14 +1,18 @@
-function WeatherService(){
-	
-	this.getWeather =  function(callWhenDone){
+function WeatherService() {
+	var url = 'http://bcw-getter.herokuapp.com/?url=';
+	var url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35'
+	var apiUrl = url + encodeURIComponent(url2);
+
+
+	this.getWeather = function (callWhenDone) {
 		var w = localStorage.getItem('weather');
-		if(w){
+		if (w) {
 			w = JSON.parse(w);
 			console.log('Weather Data:', w)
 			return callWhenDone(w)
 		}
-		
-		$.get('http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35', function(res){
+
+		$.get(apiUrl, function (res) {
 			localStorage.setItem('weather', JSON.stringify(res))
 			callWhenDone(res);
 		})
