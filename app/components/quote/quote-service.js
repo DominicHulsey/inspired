eb7d66bf7979e724e23a03dcffdbfcccde80dbab
Jake Overall <joverall22@gmail.com>
2016-11-10 04:07:31 -0700

    Update quote-service.js

--- app/components/quote/quote-service.js
+++ app/components/quote/quote-service.js
@@ -1,5 +1,5 @@
 function QuoteService(){
-	var url = 'http://bcw-getter.herokuapp.com/?url=';
+	var url = '//bcw-getter.herokuapp.com/?url=';
 	var url2 = 'http://quotesondesign.com/api/3.0/api-3.0.json';
 	var apiUrl = url + encodeURIComponent(url2);
 	//Do Not Edit above we have to go through the bcw-getter to access this api
