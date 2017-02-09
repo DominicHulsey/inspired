80d1a8f58f779580001f94d57ed464dad48fc33b
Jake Overall <joverall22@gmail.com>
2017-02-09 03:56:25 -0700

    Update weather-service.js

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -1,5 +1,5 @@
 function WeatherService() {
-	var url = 'http://bcw-getter.herokuapp.com/?url=';
+	var url = 'https://bcw-getter.herokuapp.com/?url=';
 	var url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35'
 	var apiUrl = url + encodeURIComponent(url2);
 
