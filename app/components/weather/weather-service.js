0664da68e05d292edd50e64ce7928e793e72b05a
Jake Overall <joverall@gmail.com>
2017-05-05 14:31:47 -0600

    formatting fix

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -3,7 +3,6 @@ function WeatherService() {
 	var url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35'
 	var apiUrl = url + encodeURIComponent(url2);
 
-
 	this.getWeather = function (callWhenDone) {
 		$.get(apiUrl, function (res) {
 			res = JSON.parse(res)
