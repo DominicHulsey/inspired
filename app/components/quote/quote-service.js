f09c1d67620a62c1b9dd6bcf3ea76c07e5d822ad
Darryl Kilzer Jr <darrylkilzer@gmail.com>
2018-11-16 11:24:59 -0700

    Update quote-service.js

--- app/components/quote/quote-service.js
+++ app/components/quote/quote-service.js
@@ -1,5 +1,5 @@
 let url = '//bcw-getter.herokuapp.com/?url=';
-let url2 = 'http://quotesondesign.com/api/3.0/api-3.0.json';
+let url2 = 'https://favqs.com/api/qotd';
 let apiUrl = url + encodeURIComponent(url2);
 //Do Not Edit above we have to go through the bcw-getter to access this api
 
